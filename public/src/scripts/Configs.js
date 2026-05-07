369334fa32a68e0862e2edac71d9d7465ed9d79c
Daniel9896 <albad869@gmail.com>
2026-05-07 16:38:54 -0500

    changes

--- public/src/scripts/Configs.js
+++ public/src/scripts/Configs.js
@@ -16,7 +16,7 @@ export const Config = {
     timeOut: null,
     timeRoutineTopBar: null,
     currentUser: null,
-    limitExport: 1500,
+    limitExport: 1000,
     timeOutExport: 200,
     idEsmeraldas: "5f13b0bc-95e7-721e-62de-90b58ce56002",
     idFloralVC: "5263e907-4553-8b4d-b9ea-c012063c1386",
