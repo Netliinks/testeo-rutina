00d08f16e438f0421f5cc89c3f9a9c4c3e0ad932
Netliinks S.A <netliinkssa@gmail.com>
2026-08-03 19:15:17 +0000

    Nuevo dominio

--- public/public/src/scripts/services/FirebaseCtrl.js
+++ public/public/src/scripts/services/FirebaseCtrl.js
@@ -33,7 +33,7 @@ export class FirebaseCtrl {
             }
             //navigator.serviceWorker.register("./public/src/scripts/services/firebase-messaging-sw.js");
             if ("serviceWorker" in navigator) {
-                const serviceWorkerRegistration = await navigator.serviceWorker.register('/netguard/firebase-messaging-sw.js', {scope: "/netguard/"}).catch((error) => {
+                const serviceWorkerRegistration = await navigator.serviceWorker.register('/firebase-messaging-sw.js', {scope: "/"}).catch((error) => {
                     console.error(`Service worker registration failed: ${error}`);
                 });
     
